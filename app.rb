4bd065abd3ac5595aad5a0a619ff8b71b61fe404
Oliver <olliepar01@gmail.com>
2019-08-23 16:48:20 +0100

    added more css and loaded all the images, the database now deletes then creates clearing the last request

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-
 require "net/http"
 require "uri"
 require "json"
@@ -7,12 +6,8 @@ require "sqlite3"
  
 class PhotoGallery < Sinatra::Base
 
-    # @@db = SQLite3::Database.new("./database.db")
-    # @@db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , likes INTEGER , user_id INT);")
-
-
     get '/' do
-    fetch(1)
+    fetch
     erb :'index'
     end
 
@@ -22,12 +17,18 @@ class PhotoGallery < Sinatra::Base
  
 end
 
-def fetch(image)
+def fetch
     @db = SQLite3::Database.new("./database.db")
+    @db.execute("DROP TABLE photos") #Deletes the old data
     @db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , likes INTEGER , user_id INT);")
-    terms = "music"
-    request = URI("https://pixabay.com/api/?key=#{ENV['PIXABAY_API_KEY']}&q=#{terms}&image_type=photo")
+    request = URI("https://pixabay.com/api/?key=#{ENV['PIXABAY_API_KEY']}&q=music&image_type=photo")
     response = Net::HTTP.get_response(request)
     resp = JSON.parse(response.body)
-    @db.execute("INSERT INTO photos(longUrl, likes, user_id) VALUES (?,?,?)",resp['hits'][0]['largeImageURL'],resp['hits'][0]['likes'],resp['hits'][0]['user_id'])
+
+    i = 0
+    while i < 10  do #Saves the first 10 photos in the database
+        res = resp['hits'][i] #Breaks down the request array
+        @db.execute("INSERT INTO photos(longUrl, likes, user_id) VALUES (?,?,?)",res['largeImageURL'],res['likes'],res['user_id'])
+        i += 1
+    end
 end
