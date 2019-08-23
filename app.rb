735b8a562225d12db05ef3c7bb4f274958e426e6
Oliver <olliepar01@gmail.com>
2019-08-23 14:36:10 +0100

    working api save to database so that the front end can be linked up

--- app.rb
+++ app.rb
@@ -3,29 +3,26 @@ require 'sinatra'
 require "net/http"
 require "uri"
 require "json"
+require "sqlite3"
  
 class PhotoGallery < Sinatra::Base
-
-    def initialize
-        @db = SQLite3::Database.new("./database.db")
-        @db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , likes INTEGER , user_id INT);")
-    end 
- 
     get '/' do
     erb :'index'
     end
 
     get '/photo' do
+    fetch(1)
     erb :'photo'
     end
  
 end
 
 def fetch(image)
+    @db = SQLite3::Database.new("./database.db")
+    @db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , likes INTEGER , user_id INT);")
     terms = "music"
     request = URI("https://pixabay.com/api/?key=#{ENV['PIXABAY_API_KEY']}&q=#{terms}&image_type=photo")
     response = Net::HTTP.get_response(request)
     resp = JSON.parse(response.body)
-    resp = resp['hits']
-    @db.execute("INSERT INTO photos(longUrl, likes, user_id) VALUES (?,?,?)",resp[0]['largeImageURL'],resp[0]['likes'],resp[0]['user_id'])
+    @db.execute("INSERT INTO photos(longUrl, likes, user_id) VALUES (?,?,?)",resp['hits'][0]['largeImageURL'],resp['hits'][0]['likes'],resp['hits'][0]['user_id'])
 end
