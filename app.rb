3dcf1f50aad1e286342551fe43867d4425c9b2d5
Oliver <olliepar01@gmail.com>
2019-08-27 14:36:32 +0100

    A user can now search for specific topics and they transfer across pages

--- app.rb
+++ app.rb
@@ -7,14 +7,21 @@ require "sqlite3"
 class PhotoGallery < Sinatra::Base
 
     get '/' do
-        fetch()
+        @type = params[:type]
+        fetch(@type)
         erb :'index'
     end
 
     get '/photo' do
-        fetch()
         @id_set = params[:id]
+        @type = params[:type]
+        fetch(@type)
         erb :'photo'
+    end 
+
+    get '/photographer' do
+        fetch()
+        erb :'photographer'
     end 
 
     not_found do
@@ -26,15 +33,15 @@ end
 def fetch(type = "music")
     @db = SQLite3::Database.new("./database.db")
     @db.execute("DROP TABLE photos") #Deletes the old data
-    @db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , user_id INT , previewURL CHAR);")
+    @db.execute("CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY , longUrl CHAR , user_id INT, user TEXT , previewURL CHAR);")
     request = URI("https://pixabay.com/api/?key=#{ENV['PIXABAY_API_KEY']}&q=#{type}&image_type=photo&orientation=horizontal")
-    response = Net::HTTP.get_response(request)
-    resp = JSON.parse(response.body)
-
+    resp = JSON.parse(Net::HTTP.get_response(request).body)
+    
     i = 0
-    while i < 9  do #Saves the first 9 photos in the database
-        res = resp['hits'][i] #Breaks down the request array
-        @db.execute("INSERT INTO photos(longUrl, user_id, previewURL) VALUES (?,?,?)",res['largeImageURL'],res['user_id'], res['previewURL'])
+    while i < resp['hits'].count()  do #Saves the first 9 photos in the database
+        res = resp['hits'][i]
+         #Breaks down the request array
+        @db.execute("INSERT INTO photos(longUrl, user_id, user, previewURL) VALUES (?,?,?,?)",res['largeImageURL'],res['user_id'], res['user'], res['previewURL'])
         i += 1
     end
 end
